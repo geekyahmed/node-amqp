465a059866d4b0625c2919843d9ec66e481d6cfe
Ahmed Bankole <thegeekyahmed@gmail.com>
2022-05-03 18:10:12 -0700

    worked on consumer

---
+++ consumer.js
@@ -0,0 +1,31 @@
+const amqp = require('amqplib/callback_api')
+
+amqp.connect('amqp://localhost', (err, connection) => {
+    if (err)
+        throw err
+
+    connection.createChannel((err, channel) => {
+        if (err)
+            throw err
+
+        let queue = 'hello';
+
+        channel.assertQueue(queue, {
+            durable: false
+        })
+
+        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+        
+        channel.consume(queue, function (msg) {
+            console.log(" [x] Received %s", msg.content.toString());
+        }, {
+            noAck: true
+        });
+    })
+
+    setTimeout(() => {
+        connection.close();
+        process.exit(0)
+    }, 500)
+})
+
